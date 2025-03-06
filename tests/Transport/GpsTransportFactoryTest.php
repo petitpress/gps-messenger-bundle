7e7b71d8d423ee43f6cbbc951fc3846c3cae1192
Andrej Hudec <pulzarraider2@gmail.com>
2025-03-06 12:37:59 +0100

    Fix undefined array key "credentialsConfig" (#41)

--- tests/Transport/GpsTransportFactoryTest.php
+++ tests/Transport/GpsTransportFactoryTest.php
@@ -4,30 +4,56 @@ declare(strict_types=1);
 
 namespace PetitPress\GpsMessengerBundle\Tests\Transport;
 
+use PetitPress\GpsMessengerBundle\Transport\GpsConfigurationResolver;
 use PetitPress\GpsMessengerBundle\Transport\GpsConfigurationResolverInterface;
+use PetitPress\GpsMessengerBundle\Transport\GpsTransport;
 use PetitPress\GpsMessengerBundle\Transport\GpsTransportFactory;
 use PHPUnit\Framework\TestCase;
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 
 class GpsTransportFactoryTest extends TestCase
 {
-    private GpsTransportFactory $subject;
-
-    protected function setUp(): void
-    {
-        $this->subject = new GpsTransportFactory(
-            $this->createMock(GpsConfigurationResolverInterface::class),
-            $this->createMock(CacheItemPoolInterface::class),
-            null
-        );
-    }
-
     /**
      * @dataProvider dsnProvider
      */
     public function testSupports(bool $expected, string $dsn): void
     {
-        static::assertSame($expected, $this->subject->supports($dsn, []));
+        $factory = new GpsTransportFactory(
+            $this->createMock(GpsConfigurationResolverInterface::class),
+            $this->createMock(CacheItemPoolInterface::class),
+            null
+        );
+
+        static::assertSame($expected, $factory->supports($dsn, []));
+    }
+
+    public function testCreateTransport(): void
+    {
+        $factory = new GpsTransportFactory(
+            new GpsConfigurationResolver(),
+            $this->createMock(CacheItemPoolInterface::class),
+            null
+        );
+
+        $transport = $factory->createTransport(
+            'gps://default',
+            [
+                'client_config' => [
+                    'projectId' => 'emulator-project',
+                    'credentials' => json_decode(
+                        '{"client_id":"emulator","client_secret":"emulator","refresh_token":"emulator","type":"authorized_user"}',
+                        true,
+                        512,
+                        JSON_THROW_ON_ERROR
+                    ),
+                ],
+            ],
+            $this->createMock(SerializerInterface::class)
+        );
+
+        /** @phpstan-ignore-next-line staticMethod.alreadyNarrowedType */
+        static::assertInstanceOf(GpsTransport::class, $transport);
     }
 
     /**
