8f1f618955544738ce40d6f113a66f089fd81c41
Ignacio Visedo Salvat <naxo8628@gmail.com>
2025-10-11 20:03:47 +0200

    add maxDeliveryAttemps & deadLetterTopic

--- src/Transport/Stamp/GpsReceivedStamp.php
+++ src/Transport/Stamp/GpsReceivedStamp.php
@@ -13,14 +13,28 @@ use Symfony\Component\Messenger\Stamp\StampInterface;
 final class GpsReceivedStamp implements StampInterface
 {
     private Message $gpsMessage;
+    private ?int $maxDeliveryAttempts = null;
+    private ?string $deadLetterTopic = null;
 
-    public function __construct(Message $gpsMessage)
+    public function __construct(Message $gpsMessage, array $subcriptionInfo)
     {
         $this->gpsMessage = $gpsMessage;
+        $this->maxDeliveryAttempts = $subcriptionInfo['deadLetterPolicy']['maxDeliveryAttempts'] ?? null;
+        $this->deadLetterTopic = $subcriptionInfo['deadLetterPolicy']['deadLetterTopic'] ?? null;
     }
 
     public function getGpsMessage(): Message
     {
         return $this->gpsMessage;
+    }
+
+    public function getMaxDeliveryAttempts(): ?int
+    {
+        return $this->maxDeliveryAttempts;
+    }
+
+    public function getDeadLetterTopic(): ?string
+    {
+        return $this->deadLetterTopic;
     }
 }
